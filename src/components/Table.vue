111e864ffb0740699e3d494c09c83c54e6981e1d
Web Developer <web.newdiscoverytech@gmail.com>
2021-11-03 10:29:41 -0600

    [ADD]: Filter, Design, TableComponent

---
+++ src/components/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <table class="table table-hover table-dark text-light">
+        <thead>
+            <tr>
+                <th v-for="(title, index) in titles" :key="index">
+                    {{ title }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(coin, index) in coins" :key="coin.id">
+                <td class="text-muted">{{ index }}</td>
+                <td>
+                    <img
+                        :src="coin.image"
+                        :alt="coin.name"
+                        style="width: 2rem"
+                        class="me-2"
+                    />
+                    <span>
+                        {{ coin.name }}
+                    </span>
+                    <span class="ms-2 text-muted text-uppercase">
+                        {{ coin.symbol }}
+                    </span>
+                </td>
+                <td>{{ coin.current_price.toLocaleString() }}</td>
+                <td
+                    :class="[
+                        coin.price_change_percentage_24h > 0
+                            ? 'text-success'
+                            : 'text-danger',
+                    ]"
+                >
+                    {{ coin.price_change_percentage_24h }}
+                </td>
+                <td>{{ coin.total_volume.toLocaleString() }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+export default {
+    props: {
+        coins:{
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
